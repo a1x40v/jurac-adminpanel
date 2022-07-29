ce21b2cbdde8d86251303d8cd8348c4afd774462
a1x40v <a1x40v@gmail.com>
2022-07-29 14:37:40 +0300

    Seed backend state

--- server/src/API/Program.cs
+++ server/src/API/Program.cs
@@ -1,10 +1,19 @@
 using API;
+using Persistence;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
         var host = CreateHostBuilder(args).Build();
+
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<ApplicationDbContext>();
+
+            await Seed.SeedData(context);
+        }
 
         await host.RunAsync();
     }
---
+++ server/src/Domain/Enums/RectabDeploymentStatus.cs
@@ -0,0 +1,8 @@
+namespace Domain.Enums
+{
+    public enum RectabDeploymentStatus
+    {
+        NotUpdated = 0,
+        Updated = 1
+    }
+}
