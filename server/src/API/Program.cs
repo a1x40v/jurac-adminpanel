f4666aa9906c774eb23c22e96e283a50fe7bd703
a1x40v <a1x40v@gmail.com>
2022-12-06 13:57:32 +0300

    Add docker-compose

--- server/src/API/Program.cs
+++ server/src/API/Program.cs
@@ -9,8 +9,14 @@ public class Program
         await host.RunAsync();
     }
 
+    public static void AddAppConfiguration(HostBuilderContext hostingContext, IConfigurationBuilder config)
+    {
+        config.AddKeyPerFile(directoryPath: "/run/secrets", optional: true);
+    }
+
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(AddAppConfiguration)
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
